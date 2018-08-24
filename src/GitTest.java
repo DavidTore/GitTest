60d89079f64a94a800097b1a3c2945471a538d02
DavidTore <35128524+DavidTore@users.noreply.github.com>
2018-08-24 17:00:45 +0800

    1st Update on GitHub after Merge

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,7 +1,5 @@
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("File A");
-        System.out.println("update on web 2nd time");
-        //Modify GitHub info.
+        System.out.println("Update on Web");
     }
 }
