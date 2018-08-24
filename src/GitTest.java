c198891f0b2a751f171f79f336c372a1bbdb3202
DavidTore <35128524+DavidTore@users.noreply.github.com>
2018-08-24 16:35:41 +0800

    update on web 3rd

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("File A");
         System.out.println("update on web 2nd time");
+        //Modify GitHub info.
     }
 }
