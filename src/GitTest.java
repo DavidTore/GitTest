0e0475b1f45a93e942ce3f0b3e29719d776515f0
DavidTore <35128524+DavidTore@users.noreply.github.com>
2018-08-24 16:29:33 +0800

    update on web 2nd time

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
     public static void main(String[] args){
         System.out.println("File A");
-        System.out.println("update on web");
+        System.out.println("update on web 2nd time");
     }
 }
